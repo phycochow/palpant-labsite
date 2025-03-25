61d96e37b3e525318c2f5fff09abc3a6247f792e
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2025-03-25 15:21:27 +1000

    Update draft

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def home():
     return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
