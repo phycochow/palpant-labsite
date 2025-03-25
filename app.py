7ccb70a0e76c2475500c334ad692d103435f29b5
Chris Siu Yeung Chow <siuyeung.chow@uqconnect.edu.au>
2025-03-25 17:09:47 +1000

    added comments

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ def home():
 if __name__ == '__main__':
     # Development server only – not used in production
     app.run(host='0.0.0.0', debug=True)
+
+# you run this file by
+# cd ~/palpant-labsite
+# source venv/bin/activate
+# gunicorn --bind 127.0.0.1:8000 app:app
