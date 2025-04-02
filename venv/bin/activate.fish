3c66f94648956bc5f8ff1471cb56062b0c59db2a
phycochow <ubuntu@ip-172-26-9-229.ap-southeast-1.compute.internal>
2025-04-02 04:26:10 +0000

    Describe what you changed

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ubuntu/flaskapp/venv
+set -gx VIRTUAL_ENV /home/ubuntu/palpant-labsite/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
